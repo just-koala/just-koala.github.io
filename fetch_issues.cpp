b38f95a1ee61496542d4815c48c72408bc7fcc8f
T0xicK0ala <toxicuskoala@gmail.com>
2024-07-17 13:54:30 +1000

    include header

--- fetch_issues.cpp
+++ fetch_issues.cpp
@@ -6,19 +6,22 @@
 #include <vector>
 #include <htmlcxx/html/ParserDom.h>
 
+using json = nlohmann::json;
+
 // configurations
 const std::string GITHUB_REPO = "just-koala/just-koala.github.io";
 const std::string GITHUB_TOKEN = getenv("KOALA_TOKEN");
 
-nlohmann::json fetch_issues(const std::string &repo)
+// Fetch issues from GitHub
+json fetch_issues(const std::string &repo)
 {
     std::string url = "https://api.github.com/repos/" + repo + "/issues";
     cpr::Response r = cpr::Get(cpr::Url{url}, cpr::Header{{"Authorization", "token " + GITHUB_TOKEN}});
-    return nlohmann::json::parse(r.text);
+    return json::parse(r.text);
 }
 
 // Generate blog posts from issues
-std::string generate_post(const nlohmann::json &issue)
+std::string generate_post(const json &issue)
 {
     std::ifstream template_file("template.html");
     std::string template_str((std::istreambuf_iterator<char>(template_file)), std::istreambuf_iterator<char>());
@@ -41,9 +44,10 @@ std::string generate_post(const nlohmann::json &issue)
     return template_str;
 }
 
+// Main function
 int main()
 {
-    nlohmann::json issues = fetch_issues(GITHUB_REPO);
+    json issues = fetch_issues(GITHUB_REPO);
     std::ofstream posts_dir("posts");
 
     for (auto &issue : issues)
@@ -58,5 +62,6 @@ int main()
             }
         }
     }
+
     return 0;
 }
